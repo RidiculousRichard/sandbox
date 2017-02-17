a96a5424ed098a9b30b23071013239b8074c2302
RidiculousRichard <RidiculousRichard@users.noreply.github.com>
2017-02-17 22:01:57 +0000

    Test badge respose to fixing up code

--- hello.go
+++ hello.go
@@ -10,7 +10,6 @@ func main() {
 
 func printHelloWorld() {
 	fmt.Println("Hello world")
-	nomber := 42
-	fmt.Println("The answer to life, the universe and everything is ....", nomber)
-	nomber = 41
+	n := 42
+	fmt.Println("The answer to life, the universe and everything is ....", n)
 }
--- hello_test.go
+++ hello_test.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"testing"
-	"fmt"
 )
 
 func TestPrintHelloWorld(t *testing.T) {
 	printHelloWorld()
-	fmt.Println((2+3)+5)
 }
