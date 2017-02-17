b40919d9abd804cb8c50541dcdc62e03a6b7a21f
RidiculousRichard <RidiculousRichard@users.noreply.github.com>
2017-02-17 20:51:35 +0000

    Bad fomatting

--- hello.go
+++ hello.go
@@ -10,4 +10,7 @@ func main() {
 
 func printHelloWorld() {
 	fmt.Println("Hello world")
+	nomber := 42
+	fmt.Println("The answer to life, the universe and everything is ....", nomber)
+	nomber = 41
 }
--- hello_test.go
+++ hello_test.go
@@ -1,7 +1,11 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"fmt"
+)
 
 func TestPrintHelloWorld(t *testing.T) {
 	printHelloWorld()
+	fmt.Println((2+3)+5)
 }
